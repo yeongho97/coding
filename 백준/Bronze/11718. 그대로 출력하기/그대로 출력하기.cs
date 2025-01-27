6d7261a8a935ccf0b9065480efd00620a85bef54
yeongho97 <87706153+yeongho97@users.noreply.github.com>
2025-01-27 15:45:33 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 52 ms, Memory: 5392 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test241229
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 100)
+            {
+                Console.WriteLine(Console.ReadLine());
+                i++;
+            }
+        }
+    }
+}
