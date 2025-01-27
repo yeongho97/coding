d8cf40f97eead1391cb6c1be6fa822e6f75917c6
yeongho97 <87706153+yeongho97@users.noreply.github.com>
2025-01-27 16:53:25 +0900

    [Bronze V] Title: 아스키 코드, Time: 52 ms, Memory: 5452 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Diagnostics;
+
+namespace Test241229
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char temp = char.Parse(Console.ReadLine());
+            Console.WriteLine((int)temp);
+        }
+    }
+}
