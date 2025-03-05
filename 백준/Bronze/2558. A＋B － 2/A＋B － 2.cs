4be9708c6d9b551057bd535608da8b733cf8005c
yeongho97 <87706153+yeongho97@users.noreply.github.com>
2025-03-05 21:21:03 +0900

    [Bronze V] Title: A+B - 2, Time: 52 ms, Memory: 5492 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Baekjoon
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(A+B);
+
+        }
+    }
+}
